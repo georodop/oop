4ab2a64e4a183558a9c60bd35abe2f7285394321
George Rodopoulos <georodop@gmail.com>
2017-12-11 09:59:13 +0000

    Minor changes

--- Tutorial/Chapter1/1.12-Chapter1-Quiz/question3/main.cpp
+++ Tutorial/Chapter1/1.12-Chapter1-Quiz/question3/main.cpp
@@ -1,3 +1,10 @@
+/*  Chapter 1 Quiz, question 3
+ *  Modify the program you wrote in #2 so that it uses a header file (named io.h) to
+ *  access the functions instead of using forward declarations directly in your code
+ *  (.cpp) files. Make sure your header file uses header guards.
+ */
+
+
 /*  Chapter 1 Quiz, question 2
  *  Modify the program you wrote in exercise #1 so that readNumber() and writeAnswer()
  *  live in a separate file called “io.cpp”. Use a forward declaration to access them 
@@ -6,20 +13,20 @@
 
 
 /*Chapter 1 Quiz, question 1
-Write a single-file program (named main.cpp) that reads two separate integers 
-from the user, adds them together, and then outputs the answer. The program 
-should use three functions:
-
-        A function named “readNumber” should be used to get (and return) a single 
-    integer from the user.
-        A function named “writeAnswer” should be used to output the answer. This 
-    function should take a single parameter and have no return value.
-        A main() function should be used to glue the above functions together. 
-
-    Hint: You do not need to write a separate function to do the adding 
-(just use operator+ directly).
-    Hint: You will need to call readNumber() twice.
-*/
+ *  Write a single-file program (named main.cpp) that reads two separate integers 
+ *  from the user, adds them together, and then outputs the answer. The program 
+ *  should use three functions:
+ *  
+ *  *   A function named “readNumber” should be used to get (and return) a single 
+ *  integer from the user.
+ *  *   A function named “writeAnswer” should be used to output the answer. This 
+ *  function should take a single parameter and have no return value.
+ *  *   A main() function should be used to glue the above functions together. 
+ *  
+ *  *   Hint: You do not need to write a separate function to do the adding 
+ *  (just use operator+ directly).
+ *  *   Hint: You will need to call readNumber() twice.
+ */
 #include <iostream>
 #include "io.h"
 
